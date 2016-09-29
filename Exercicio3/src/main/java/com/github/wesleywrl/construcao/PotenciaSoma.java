a9210648a4203ac9d04e24d0cf9a59a11b27a415
aluno <larissac.lopes@hotmail.com>
2016-09-28 21:42:03 -0300

    exercicio 3

---
+++ Exercicio3/src/main/java/com/github/wesleywrl/construcao/PotenciaSoma.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.wesleywrl.construcao;
+
+/**
+ *
+ * @author Administrador
+ */
+public class PotenciaSoma {
+    public static void main(String[] args){
+        System.out.println(Exercicio3.produtoPotencia(3, 2));
+    }
+}
---
+++ Exercicio3/src/test/java/com/github/wesleywrl/construcao/PotenciaSomaTest.java
@@ -0,0 +1,14 @@
+package com.github.wesleywrl.construcao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PotenciaSomaTest {
+
+    @Test
+    public void umCasoTrivial() {
+        assertEquals(9, PotenciaSoma(3, 2));
+    }
+}
+
