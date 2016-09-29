4e5b7ca480499dbc394cba31b0a60469530e9197
aluno <larissac.lopes@hotmail.com>
2016-09-28 21:31:17 -0300

    Potencia usando soma

---
+++ Exercicio3/src/main/java/com/github/wesleywrl/construcao/Exercicio3.java
@@ -0,0 +1,21 @@
+
+package com.github.wesleywrl.construcao;
+
+
+public class Exercicio3 {
+
+   
+    public static int produtoPotencia(int x, int y) {
+        int potencia = 0, i;
+        if(x >= 0 && y >= 0){
+          potencia = 1;
+          i = 1;
+          while(i <= y){
+              potencia = potencia * x;
+              i = i + 1;
+          }
+        }
+      return potencia;  
+    }
+    
+}
